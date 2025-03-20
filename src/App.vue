e2e1fdd4e1212d3b356d4c649b96898eda0b1fd7
Hcki186 <hackicrew186@gmail.com>
2025-03-20 21:58:23 +0100

    adding welcome menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Sidebar from './components/sections/sideMenu.vue'
+import welcomeSection from './components/sections/welcomeSection.vue'
 </script>
 
 <template>
   <div>
     <Sidebar />
+    <welcomeSection />
   </div>
 </template>
---
+++ src/components/sections/welcomeSection.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="welcome-menu">
+    <img src="@/assets/elements/images/logo.svg" alt="Logo" class="logo" />
+  </div>
+</template>
+
+<style lang="scss"></style>
