6ce6385532bbc6b67706062b6e97a3a9ffcbdb49
Hcki186 <hackicrew186@gmail.com>
2025-03-21 21:59:12 +0100

    about compeny header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Sidebar from './components/sections/sideMenu.vue'
 import welcomeSection from './components/sections/welcomeSection.vue'
+import aboutCompeny from './components/sections/aboutCompeny.vue'
 </script>
 
 <template>
   <div>
     <Sidebar />
     <welcomeSection />
+    <aboutCompeny />
   </div>
 </template>
